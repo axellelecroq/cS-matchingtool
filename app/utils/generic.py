b03d2fd254b324997924c73fb8cd2f246279de91
axellelecroq <axelle.lecroq@chartes.psl.eu>
2021-06-21 11:34:50 +0200

    Clean sourceDesc and respStmt of output file

--- app/utils/generic.py
+++ app/utils/generic.py
@@ -33,15 +33,43 @@ def make_cmif(file:str):
     tree = ET.parse(file)
     root = tree.getroot()
 
-    toremove= []
+    letter_toremove= []
+    bibl_tokeep = []
+    bibl_remove = []
+    resp=[]
+    resp_toremove=[]
 
     for child in root[0][1]:
+        # If a kalliope link was stored in @corresp, it's mean that's a match.
+        # If not, the letter should be remove of the CMIF file
         if "corresp" not in child.attrib:
-            toremove.append(child)
+            letter_toremove.append(child)
+        else :
+            # Source of matching letters is kept in a list. 
+            if child.attrib['source'] not in bibl_tokeep:
+                source_id = child.attrib['source'].split('#')[1]
+                bibl_tokeep.append(source_id)
+    
+    # Clean the sourceDesc (bibliographie)
+    for child in root[0][0][2]:
+        if child.attrib['{http://www.w3.org/XML/1998/namespace}id'] not in bibl_tokeep:
+            bibl_remove.append(child)
+    for child in bibl_remove:
+        root[0][0][2].remove(child)
 
-    for child in toremove:
+    # Clean the respStmt (institutions)
+    for child in root[0][0][0][1]:
+        if child.text not in resp:
+            resp.append(child.text)
+        else :resp_toremove.append(child)
+    for child in resp_toremove:
+        root[0][0][0][1].remove(child)    
+
+    # Clean the profileDesc (letters)
+    for child in letter_toremove:
         root[0][1].remove(child)
     
+    # Store the clean file
     tree.write('app/data/output.xml',
            xml_declaration=True, encoding='utf-8',
            method="xml")
